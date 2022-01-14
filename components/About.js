4391cc6ea694a01b60ccda1e76528eb39bd9f1fc
Arthur Karambizi <arthurkarambizi@gmail.com>
2022-01-14 00:37:31 -0500

    made changes to the precommit

--- components/About.js
+++ components/About.js
@@ -7,7 +7,7 @@ export default function About() {
     return (
         <AppContainer id="about">
             <article>
-                <PageTitle title="About Me."></PageTitle>
+                <PageTitle title="About Me." />
                 <p>Hello again! My name is Arthur Karambizi. I love building websites And I also enjoy other activities like Soccer ,music and art.</p>
                 <figure>
                     <img src="https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="Arthur Karambizi" />
@@ -16,17 +16,17 @@ export default function About() {
                         <p>Software Engineer</p>
                         <ul>
                             <li>
-                                <a href="mailto:arthurkarambizi@gmail.com" target="_blank" rel="noopener" aria-label="Email link">
+                                <a href="mailto:arthurkarambizi@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Email link">
                                     <EmailIcon />
                                 </a>
                             </li>
                             <li>
-                                <a href="https://www.linkedin.com/in/arthur-karambizi-7a0797143/" target="_blank" rel="noopener" aria-label="Linkedin link">
+                                <a href="https://www.linkedin.com/in/arthur-karambizi-7a0797143/" target="_blank" rel="noopener noreferrer" aria-label="Linkedin link">
                                     <LinkedinIcon />
                                 </a>
                             </li>
                             <li>
-                                <a href="https://github.com/arthurkarambizi" target="_blank" rel="noopener" aria-label="Github link">
+                                <a href="https://github.com/arthurkarambizi" target="_blank" rel="noopener noreferrer" aria-label="Github link">
                                     <GithubIcon />
                                 </a>
                             </li>
